fec893088ddfd6a76f5bd4444d2d23cbf4ed45b7
Lennart <115736797+lnart@users.noreply.github.com>
2023-05-24 10:40:38 +0200

    refactoring: extract code from method

--- src/controller/trainingController.ts
+++ src/controller/trainingController.ts
@@ -17,28 +17,43 @@ export function createArrayOfCombos(requestBody) {
   return arrayOfComboObjects;
 }
 
-export function addCombosRandomly(arrayOfComboObjects, numCombosToAdd:number) {
+export function addCombosRandomly(arrayOfComboObjects, numCombosToAdd:number){
+  const cumulativeProbabilities = calculateCumulativeProbabilities(arrayOfComboObjects);
+
+  const addedCombos = []
+
+  for (let i = 0; i < numCombosToAdd; i++) {
+    const selectedCombo = selectComboRandomly(arrayOfComboObjects, cumulativeProbabilities);
+    addedCombos.push(selectedCombo);
+  }
+
+  return addedCombos;
+
+}
+
+
+function calculateCumulativeProbabilities(arrayOfComboObjects) {
   let cumulativeProbabilities = [];
   let cumulativeProbability = 0;
   for (let i = 0; i < arrayOfComboObjects.length; i++) {
     cumulativeProbability += arrayOfComboObjects[i].probability;
     cumulativeProbabilities.push(cumulativeProbability);
   }
+  return cumulativeProbabilities;
+}
 
-  let addedCombos = [];
-  for (let i = 0; i < numCombosToAdd; i++) {
-    const random = Math.random();
-    let selectedCombo = null;
 
-    for (let j = 0; j < arrayOfComboObjects.length; j++) {
-      if (random < cumulativeProbabilities[j]) {
-        selectedCombo = arrayOfComboObjects[j].combo;
-        break;
-      }
+function selectComboRandomly(arrayOfComboObjects, cumulativeProbabilities) {
+  const random = Math.random();
+  let selectedCombo = null;
+
+  for (let j = 0; j < arrayOfComboObjects.length; j++) {
+    if (random < cumulativeProbabilities[j]) {
+      selectedCombo = arrayOfComboObjects[j].combo;
+      break;
     }
-
-    addedCombos.push(selectedCombo);
   }
 
-  return addedCombos;
+  return selectedCombo;
 }
+
