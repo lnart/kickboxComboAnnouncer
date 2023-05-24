4136f35026a5ec0ca51528cf0e7f70d2054e70a9
Lennart <115736797+lnart@users.noreply.github.com>
2023-05-24 10:32:05 +0200

    refactoring: rename symbols to show intend

--- src/controller/trainingController.ts
+++ src/controller/trainingController.ts
@@ -1,45 +1,42 @@
 import combis from "./comboController";
 
+
 export function createArrayOfCombos(requestBody) {
-  const arrayOfCombos = [];
+  const arrayOfComboObjects = [];
   delete requestBody.minutes;
   const length = Object.keys(requestBody).length;
   for (const key in requestBody) {
     if (requestBody.hasOwnProperty(key)) {
       const value = requestBody[key];
-      arrayOfCombos.push({
+      arrayOfComboObjects.push({
         combo: value,
         probability: combis[value]["probabilities"][length],
       });
     }
   }
-  return arrayOfCombos;
+  return arrayOfComboObjects;
 }
 
-export function addCombosRandomly(data, numCombosToAdd) {
-  // Calculate cumulative probabilities
+export function addCombosRandomly(arrayOfComboObjects, numCombosToAdd:number) {
   let cumulativeProbabilities = [];
   let cumulativeProbability = 0;
-  for (let i = 0; i < data.length; i++) {
-    cumulativeProbability += data[i].probability;
+  for (let i = 0; i < arrayOfComboObjects.length; i++) {
+    cumulativeProbability += arrayOfComboObjects[i].probability;
     cumulativeProbabilities.push(cumulativeProbability);
   }
 
-  // Add combos randomly
   let addedCombos = [];
   for (let i = 0; i < numCombosToAdd; i++) {
     const random = Math.random();
     let selectedCombo = null;
 
-    // Find the combo based on the random number
-    for (let j = 0; j < data.length; j++) {
+    for (let j = 0; j < arrayOfComboObjects.length; j++) {
       if (random < cumulativeProbabilities[j]) {
-        selectedCombo = data[j].combo;
+        selectedCombo = arrayOfComboObjects[j].combo;
         break;
       }
     }
 
-    // Add the selected combo to the array
     addedCombos.push(selectedCombo);
   }
 
